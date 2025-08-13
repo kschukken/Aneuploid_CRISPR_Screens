feb15a4fd190528ecd0e084727ad8b9bc2cd8845
Klaske M Schukken <86061155+kschukken@users.noreply.github.com>
2025-08-13 15:18:55 -0400

    DG Screen 4- A2780

---
+++ mle-process-unique-controls.sh
@@ -0,0 +1,3 @@
+mageck mle -k count-A2780-Cont1-i.f.csv -d matrixCont1.txt -n mle_count-A2780-Cont1 &
+mageck mle -k count-A2780-Ts2-i.f.csv -d matrixTs2.txt -n mle_count-A2780-Ts2 &
+mageck mle -k count-A2780-Ts10-i.f.csv -d matrixTs10.txt -n mle_count-A2780-Ts10 &
